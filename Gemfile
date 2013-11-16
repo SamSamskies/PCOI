08bd776e699a272eb2988c932abb641f4dfeba74
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-11-15 21:33:29 -0800

    Landing page ready to go

--- Gemfile
+++ Gemfile
@@ -18,7 +18,6 @@ end
 
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 PCOI::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +40,7 @@ PCOI::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
