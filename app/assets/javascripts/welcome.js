a6126cb78b54647888ce61177fb4a3068740a175
theguice <giudici@airbears2-10-142-139-70.airbears2.1918.berkeley.edu>
2013-11-15 23:50:58 -0800

    ui

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,1 +1,38 @@
+$(document).ready(function() {
+
+	console.log('test');
+	//global variable _map stores map object
+	_map = initializeMap();
+	loadData();
+
+});
+
+function initializeMap() {
+	//return map object
+
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+/*
+
+	var mapOptions = {
+          center: new google.maps.LatLng(42.2353083, -71.0012016),
+          zoom: 2,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          styles: [ { "featureType": "landscape.natural.terrain" },{ "elementType": "geometry.fill", "stylers": [ { "saturation": 6 }, { "gamma": 0.28 }, { "lightness": 24 }, { "hue": "#ff0077" }, { "weight": 0.8 } ] },{ "featureType": "road.local", "stylers": [ { "visibility": "simplified" } ] } ]
+
+        };
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+*/
+    return map;
+}
+
+function loadData() {
+	console.log('loadData');
+}
 
