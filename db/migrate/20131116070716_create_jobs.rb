52b7e63f9d9c487bbc1d4e0f8c3bcfa12f564686
Josh <josh@jgo.(none)>
2013-11-16 03:56:29 -0800

    create_jobs conflict

--- db/migrate/20131116070716_create_jobs.rb
+++ db/migrate/20131116070716_create_jobs.rb
@@ -4,17 +4,13 @@ class CreateJobs < ActiveRecord::Migration
       t.string :application_deadline
       t.string :departure_date
       t.string :description
-<<<<<<< HEAD
       t.string :notification_date
       t.integer :open_positions
       t.text :physical_requirements
       t.integer :quarter
       t.text :skills
       t.string :year
-=======
       t.integer :country_id
->>>>>>> master
-
       t.belongs_to :sector
       t.timestamps
     end
