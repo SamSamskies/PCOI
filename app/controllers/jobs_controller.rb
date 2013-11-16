b7a0c489ebe6c3cee258dad9c38f1903cb51dfca
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-11-16 08:52:13 -0800

    Order jobs by need

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   def index
 
-    @filtered_jobs = Job.filter(Sector.find_by(id: params[:sector_id]), Region.find_by(id: params[:region_id]))
+    @filtered_jobs = Job.filter(Sector.find_by(id: params[:sector_id]), Region.find_by(id: params[:region_id])).order(:need)
   end
 end
