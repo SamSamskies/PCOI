fa43f87e5074cc99367d9da5602c918483db5b75
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-11-16 07:43:23 -0800

    Consider all possible scenarios for filtering jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,10 +3,15 @@ class Job < ActiveRecord::Base
   belongs_to :sector
 
   def self.filter(sector, region)
-    return all unless (sector && region)
+    if sector && region.nil?
+      return sector.jobs
 
-    sector_jobs = sector.jobs
-    region_jobs = region.jobs
+    elsif region && sector.nil?
+      return region.jobs
+
+    elsif sector.nil? && region.nil?
+      return all
+
     sector_jobs - region_jobs
   end
 end
